2e002bae2f5d936aff08991c67bf88df7223745d
Girish310231258 <girish.kumar.p@philips.com>
2024-09-27 21:04:05 +0530

    ReduceCC1

--- monitor.cpp
+++ monitor.cpp
@@ -14,19 +14,36 @@ void printWarningMessage() {
     }
 }
 
+bool isTemperatureCritical(float temperature) {
+    return temperature > 102 || temperature < 95;
+}
+
+bool isPulseRateOutOfRange(float pulseRate) {
+    return pulseRate < 60 || pulseRate > 100;
+}
+
+bool isSpo2OutOfRange(float spo2) {
+    return spo2 < 90;
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-    if (temperature > 102 || temperature < 95) {
+    if (isTemperatureCritical(temperature)) {
         cout << "Temperature is critical!\n";
         printWarningMessage();
         return 0;
-    } else if (pulseRate < 60 || pulseRate > 100) {
+    }
+
+    if (isPulseRateOutOfRange(pulseRate)) {
         cout << "Pulse Rate is out of range!\n";
         printWarningMessage();
         return 0;
-    } else if (spo2 < 90) {
+    }
+
+    if (isSpo2OutOfRange(spo2)) {
         cout << "Oxygen Saturation out of range!\n";
         printWarningMessage();
         return 0;
     }
+
     return 1;
 }
